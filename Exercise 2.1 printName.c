4f6f96b6a079816ee82858e821461d1c1be97333
赵子豪 <zihao13969918264@hotmail.com>
2017-06-06 22:00:44 +0800

    Create Exercise 2.1 printName.c

---
+++ Exercise 2.1 printName.c
@@ -0,0 +1,18 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月6号
+*/
+
+
+
+/*	printName.c -- use printf() call to print my name in different way	*/
+#include <stdio.h>
+int main(void)
+{
+	printf("zihao zhao.\n");
+	printf("zihao \nzhao \n");
+	printf("zihao ");
+	printf("zhao");
+
+	return 0;
+}
