0c27f693a6200880de9539d2502b076dc9f569a2
赵子豪 <zihao13969918264@hotmail.com>
2017-06-07 18:12:43 +0800

    Create Exercise 2.2 printNameAndAddress.c

---
+++ Exercise 2.2 printNameAndAddress.c
@@ -0,0 +1,16 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月7日
+*/
+
+
+
+/*	printNameAndAddress.c	-- print your name and address	*/
+#include <stdio.h>
+int main(void)
+{
+	printf("zhao zihao \n");
+	printf("China\n");
+
+	return 0;
+}
