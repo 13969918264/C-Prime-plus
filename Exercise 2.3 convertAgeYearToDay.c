948a5fe97cf6faa706f9d0767c3b9a8926d68712
赵子豪 <zihao13969918264@hotmail.com>
2017-06-07 18:24:11 +0800

    Create Exercise 2.3 convertAgeYearToDay.c

---
+++ Exercise 2.3 convertAgeYearToDay.c
@@ -0,0 +1,18 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月7号
+*/
+
+
+
+/*	convertAgeYearToDay.c	--	convert your age in years to days	*/
+#include <stdio.h>
+int main(void)
+{
+	int age = 21;
+
+	printf("My age in years is %d.\n", age);
+	printf("My age in days is %d.\n", age * 365);
+
+	return 0;
+}
