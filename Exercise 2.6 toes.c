1be27bbded0f72bc4f7f7935a0687b3fc3310389
赵子豪 <zihao13969918264@hotmail.com>
2017-06-09 10:36:11 +0800

    Create Exercise 2.6 toes.c

---
+++ Exercise 2.6 toes.c
@@ -0,0 +1,20 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月9号
+*/
+
+
+
+/*	toes.c	--	print the number of toes and twice toes and so on	*/
+#include <stdio.h>
+int main(void)
+{
+	int toes;
+	toes = 10;
+
+	printf("toes is %d\n", toes);
+	printf("twice toes is %d\n", toes * 2);
+	printf("toes squared is %d\n", toes * toes);
+
+	return 0;
+}
