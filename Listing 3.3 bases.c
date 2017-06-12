a0fc1862dba4fb4cb1bb06e6aefb2052ef475ffb
赵子豪 <zihao13969918264@hotmail.com>
2017-06-12 11:55:03 +0800

    Create Listing 3.3 bases.c

---
+++ Listing 3.3 bases.c
@@ -0,0 +1,18 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月12号
+*/
+
+
+
+/*	bases.c -- prints 100 in decimal, octal, and hex	*/
+#include <stdio.h>
+int main(void)
+{
+	int x = 100;
+
+	printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
+	printf("dec = %#d; octal = %#o; hex = %#x\n", x, x, x);
+
+	return 0;
+}
