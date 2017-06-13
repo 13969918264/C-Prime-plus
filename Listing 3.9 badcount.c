e8c83d24184ef4a76271b571602047da25c2db8e
赵子豪 <zihao13969918264@hotmail.com>
2017-06-13 16:53:06 +0800

    Create Listing 3.9 badcount.c

---
+++ Listing 3.9 badcount.c
@@ -0,0 +1,22 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月13号
+*/
+
+
+
+/*	badcount.c -- incorrect argument counts	*/
+#include <stdio.h>
+int main(void)
+{
+	int n = 4;
+	int m = 5;
+	float f = 7.0f;
+	float g = 8.0f;
+
+	printf("%d\n", n, m);			/*	too many arguments	*/
+	printf("%d %d %d\n", n);			/*	too few arguments	*/
+	printf("%d %d\n", n, m);			/*	wrong kind of values	*/
+
+	return 0;
+}
