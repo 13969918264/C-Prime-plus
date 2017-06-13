6ca2dbfcdf3f373c5c78a624e53ba4868029fa52
赵子豪 <zihao13969918264@hotmail.com>
2017-06-13 21:09:20 +0800

    Create Exercise 3.3 sound.c

---
+++ Exercise 3.3 sound.c
@@ -0,0 +1,20 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月13号
+*/
+
+
+
+/*	sound.c -- an alert with some sentence	*/
+#include <stdio.h>
+int main(void)
+{
+	/*	the alert of ascii is unknown	*/
+	char beep = "\7";
+
+	printf("%c\n", beep);
+	printf("Startled by the sudden sound, Sally shouted.\n");
+	printf("\"By the Great Pumpkin, what was that!\"\n");
+
+	return 0;
+}
