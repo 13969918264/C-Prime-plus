d71e48fc5e91f97f2f6135dfd58f4522d458f7e5
赵子豪 <zihao13969918264@hotmail.com>
2017-06-13 17:26:48 +0800

    Update Listing 3.9 badcount.c

--- Listing 3.9 badcount.c
+++ Listing 3.9 badcount.c
@@ -5,18 +5,19 @@
 
 
 
-/*	badcount.c -- incorrect argument counts	*/
+/*	escape.c -- uses escape characters	*/
 #include <stdio.h>
 int main(void)
 {
-	int n = 4;
-	int m = 5;
-	float f = 7.0f;
-	float g = 8.0f;
+	float salary;
 
-	printf("%d\n", n, m);			/*	too many arguments	*/
-	printf("%d %d %d\n", n);			/*	too few arguments	*/
-	printf("%d %d\n", n, m);			/*	wrong kind of values	*/
+	printf("\aEnter your desires monthly salary:");			/*	1	*/
+	printf(" $_______\b\b\b\b\b\b\b");			/*	2	*/
+	scanf("%f", &salary);
+	/*	the '\n' excape sequence next line will operative so the display in the screen is not like the textbook	*/
+	printf("\n\t$%.2f a month is $%.2f a year.", salary, salary * 12.0);			/*	3	*/
+	/*	printf("\t$%.2f a month is $%.2f a year.", salary, salary * 12.0);	*/ // this output is like the textbook
+	printf("\rGee!\n");			/*	4	*/
 
 	return 0;
 }
