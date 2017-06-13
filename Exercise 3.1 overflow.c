09de7e197b21f340904b2cc785e223c5e5293030
赵子豪 <zihao13969918264@hotmail.com>
2017-06-13 21:45:48 +0800

    Create Exercise 3.1 overflow.c

---
+++ Exercise 3.1 overflow.c
@@ -0,0 +1,25 @@
+/*
+	Name: 赵子豪
+	Date: 2017年6月13号
+*/
+
+
+
+/*	overflow.c -- a program have some overflow	*/
+#include <stdio.h>
+int main(void)
+{
+	/*
+		I do not know the maxValue of these types
+		so the program is not complete
+	*/
+	int overFlowInt = ;
+	float overFlowFloat = ;
+	float underFlowFloat = ;
+
+	printf("The integer overflow is %d.\n", overFlowInt);
+	printf("The float-point overflow is %f.\n", overFlowFloat);
+	printf("The float-point underflow is %f.\n", underFlowFloat);
+
+	return 0;
+}
