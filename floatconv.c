882bb964ffc69f8b708becf97aeb8a4bba421ba2
赵子豪 <zihao13969918264@hotmail.com>
2017-08-13 20:20:04 +0800

    Add files via upload

---
+++ floatconv.c
@@ -0,0 +1,15 @@
+/*	floatconv.c -- mismatched floating-point conversation	*/
+#include <stdio.h>
+int main(void)
+{
+	float n1 = 3.0;
+	double n2 = 3.0;
+	long n3 = 2000000000;
+	long n4 = 1234567890;
+
+	printf("%.1e %.1e %.1e %.1e\n", n1, n2, n3, n4);			// In windows there be some differences
+	printf("%ld %ld\n", n3, n4);
+	printf("%ld %ld %ld %ld\n", n1, n2, n3, n4);
+
+	return 0;
+}
