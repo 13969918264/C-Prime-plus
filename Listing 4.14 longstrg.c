f1f14ff69079a61663cc27966b8627345c9a0d65
赵子豪 <zihao13969918264@hotmail.com>
2017-08-15 14:34:29 +0800

    Create Listing 4.14 longstrg.c

---
+++ Listing 4.14 longstrg.c
@@ -0,0 +1,17 @@
+/*
+	Name: 赵子豪(Zihao Zhao)
+	Date: 2017年8月15日
+*/
+/*	longstrg.c -- printing long strings	*/
+#include <stdio.h>
+int main(void)
+{
+	printf("Here's one way to print a ");
+	printf("long string.\n");
+	printf("Here's another way to print a \
+long string.\n");			// the statement must be the far left.
+	printf("Here's the newest way to print a "
+		  "long string.\n");			/*	ANSI C	*/
+
+	return 0;
+}
