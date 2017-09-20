063b382777911854aceaf5411cef5ca9163c1661
赵子豪 <zihao13969918264@hotmail.com>
2017-09-20 12:01:58 +0800

    Create Listing 5.3 golf.c

---
+++ Listing 5.3 golf.c
@@ -0,0 +1,16 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年9月20日
+*/
+/*	golf.c -- golf tournament scorecard	*/
+#include <stdio.h>
+int main(void)
+{
+	int jane, tarzan, cheeta;
+
+	cheeta = tarzan = jane = 68;
+	printf("			cheeta	tarzan	jane\n");
+	printf("First round score 	%4d %8d %8d\n", cheeta, tarzan, jane);
+
+	return 0;
+}
