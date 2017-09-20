7dc7c6d14ec0f2e75cdba04541273c059ae8569e
赵子豪 <zihao13969918264@hotmail.com>
2017-09-20 10:29:54 +0800

    Create listing 5.1 The shoes1.c

---
+++ listing 5.1 The shoes1.c
@@ -0,0 +1,19 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年9月20日
+*/
+/*	shoes1.c --	converts a shoe size to inches	*/
+#include <stdio.h>
+#define ADJUST 7.31			//	one kind of symbolic constant.
+int main(void)
+{
+	const double SCALE = 0.333;			//	another kind of symbolic constant
+	double shoe, foot;
+
+	shoe = 9.0;
+	foot =SCALE * shoe + ADJUST;
+	printf("Shoe size (men's)    foot length\n");
+	printf("%10.1f %15.2f inches\n", shoe, foot);
+
+	return 0;
+}
