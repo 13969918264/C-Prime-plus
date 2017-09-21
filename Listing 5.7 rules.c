bb36b4b98a7dea7e63f5052420ed211312e10e9d
赵子豪 <zihao13969918264@hotmail.com>
2017-09-21 12:29:59 +0800

    Update Listing 5.7 rules.c

--- Listing 5.7 rules.c
+++ Listing 5.7 rules.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int top, score;
 
-	top = score = -(2 + 5) * 6 + (4 + 3) * (2 + 3);
+	top = score = -(2 + 5) * 6 + (4 + 3 * (2 + 3));
 	printf("top = %d, score = %d\n", top, score);
 
 	return 0;
