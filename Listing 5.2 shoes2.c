a22c9fceb5b8c104d4e77dbb4a2f73c99ee56d1e
赵子豪 <zihao13969918264@hotmail.com>
2017-09-21 16:06:26 +0800

    Update Listing 5.2 shoes2.c

--- Listing 5.2 shoes2.c
+++ Listing 5.2 shoes2.c
@@ -11,6 +11,14 @@ int main(void)
 	double shoe, foot;
 
 	printf("Shoe size (men's)   foot length\n");
+	/*
+		shoe = 2.0;
+		while (++shoe < 18.5)
+		{
+			foot = SCALE * shoe + ADJUST;
+			printf("%10.1f %20.2f inches\n", shoe, foot);
+		}
+	*/
 	shoe = 3.0;
 	while (shoe < 18.5)			/*	starting the while loop	*/
 	{
