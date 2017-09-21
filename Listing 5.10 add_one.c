3cc5ee485ffa0972a0314ac2b11b46302b885727
赵子豪 <zihao13969918264@hotmail.com>
2017-09-21 15:57:46 +0800

    Create Listing 5.10 add_one.c

---
+++ Listing 5.10 add_one.c
@@ -0,0 +1,19 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年9月21日
+*/
+/*	add_one.c -- incrementing: prefix and postfix	*/
+#include <stdio.h>
+int main(void)
+{
+	int ultra = 0, super = 0;
+
+	while (super < 5)
+	{
+		super++;
+		++ultra;
+		printf("super = %d, ultra = %d \n", super, ultra);
+	}
+
+	return 0;
+}
