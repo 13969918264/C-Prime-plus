4a2edc32a5ab50298ae888fc4fa894c66daac0a8
赵子豪 <zihao13969918264@hotmail.com>
2017-09-22 08:08:06 +0800

    Update Listing 5.13 addemup.c

--- Listing 5.13 addemup.c
+++ Listing 5.13 addemup.c
@@ -11,6 +11,11 @@ int main(void)			/*	finds sum of first 20 integers	*/
 	count = 0;			/*	assignment statement	*/
 	sum = 0;			/*	ditto	*/
 	while (count++ < 20)			/*	while	*/
+	/*	
+		当count = 19时，小于20，继续执行循环，
+		且同时count加1，则此时 count为20，故此次循环时count的值已经为20，sum为1到20的和；
+		当用++count时，先自增，即先为19增加到20，条件不满足，则不执行循环，sum为1到19的和
+	*/
 	{
 		sum = sum + count;			/*	statement	*/
 	}
