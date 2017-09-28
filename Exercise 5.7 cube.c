96de53723483bd57633f31b7549cda7482c2c6a3
赵子豪 <zihao13969918264@hotmail.com>
2017-09-28 10:47:48 +0800

    Create Exercise 5.7 cube.c

---
+++ Exercise 5.7 cube.c
@@ -0,0 +1,34 @@
+/*
+	error: fail to run or somewhere wrong i do not know.
+*/
+
+
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年9月28日
+*/
+/*	cube.c -- read a number and cube it	*/
+#include <stdio.h>
+
+//void cube(double n);
+
+int main(void)
+{
+	double number;
+
+	printf("Enter a number, the program will cube it.\n");
+	scanf("%f", &number);
+	printf("%f.\n", number);
+	//cube(number);
+
+	return 0;
+}
+
+
+/*
+void cube(double n)
+{
+	printf("%f cubed %f.\n", n, n * n * n);
+
+}
+*/
