d12cd3ed905c3c20d21be58b34bf8c98fe33016e
赵子豪 <zihao13969918264@hotmail.com>
2017-10-09 21:13:40 +0800

    Update Exercise 5.9 temperature.c

--- Exercise 5.9 temperature.c
+++ Exercise 5.9 temperature.c
@@ -1,3 +1,53 @@
+update:
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年9月28日
+*/
+/*
+	temperature.c --
+	read a Fahrenheit temperature and convert to Celsius and Kelvin by using function
+*/
+#include <stdio.h>
+
+void Temperatures(double Fahrenheit);
+int main(void)
+{
+	double Fahrenheit;
+	int status;
+
+	printf("Enter a Fahrenheit temperature.\n");
+	printf("If you enter 'q' or other nonnumeric, it quit.\n");
+	status = scanf("%lf", &Fahrenheit);
+	while (status == 1)
+	{
+		Temperatures(Fahrenheit);
+		scanf("%lf", &Fahrenheit);
+	}
+	printf("You enter a nonnumeric, the program is over.\n");
+
+	return 0;
+}
+
+
+
+void Temperatures(double Fahrenheit)
+{
+	double Celsius, Kelvin;
+	const double RATE = 5.0 / 9.0;
+	const double MINUS = 32.0;
+	const double PLUS = 273.16;
+	Celsius = RATE * (Fahrenheit - MINUS);
+	Kelvin = Celsius + PLUS;
+
+	printf("Temperature in Fahrenheit is %9.2lf.\n", Fahrenheit);
+	printf("Temperature in Celsius is %9.2lf.\n", Celsius);
+	printf("Temperature in Kelvin is %9.2lf.\n", Kelvin);
+}
+
+
+
+update: bug is found.
+	in codeblock and mingw, scanf() use %lf to read a doulble number.
 /*
 	error: different input same output
 */
