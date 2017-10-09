c1cae9ef1d7ec7fcaa1bd6973d17d2ab439cb08c
赵子豪 <zihao13969918264@hotmail.com>
2017-10-09 21:03:21 +0800

    Update Exercise 5.7 cube.c

--- Exercise 5.7 cube.c
+++ Exercise 5.7 cube.c
@@ -1,3 +1,35 @@
+update:
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月9日
+*/
+/*	cube.c -- cube the number by using a function you designed	*/
+#include <stdio.h>
+
+void cube(double n);
+int main(void)
+{
+	double number;
+
+	printf("Please enter a number.\n");			//	why i use %f is wrong?
+	scanf("%lf", &number);
+	cube(number);
+
+	return 0;
+}
+
+
+
+void cube(double n)
+{
+	printf("You enter %lf, cubed %.lf.\n", n, n * n * n);
+}
+
+
+
+the follow is wrong , when i encounter a error, i do not know what happened with it
+so i try it in some way, i knew it wrong.
+i don't delete it.
 /*
 	error:
 			fail to run or somewhere wrong i do not know.
