2b44cb14b860fdc3b1c8b895b5f1aeca5d7cef62
赵子豪 <zihao13969918264@hotmail.com>
2017-10-10 12:50:22 +0800

    Create Listing 6.11 sweetie2.c

---
+++ Listing 6.11 sweetie2.c
@@ -0,0 +1,16 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月10日
+*/
+//	sweetie2.c -- a counting loop using for
+#include <stdio.h>
+int main(void)
+{
+	const int NUMBER = 22;
+	int count;
+
+	for (count = 1; count <= NUMBER; count++)
+		printf("Be my Valentine!\n");
+
+	return 0;
+}
