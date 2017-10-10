5a02f241204c02561fcf4a01f44e67692869804b
赵子豪 <zihao13969918264@hotmail.com>
2017-10-10 11:13:26 +0800

    Create Listing 6.8 boolean.c

---
+++ Listing 6.8 boolean.c
@@ -0,0 +1,25 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月10日
+*/
+//	boolean.c -- using a _Bool variable
+#include <stdio.h>
+int main(void)
+{
+	long num;
+	long sum = 0L;
+	_Bool input_is_good;
+
+	printf("Please enter an integer to be summed ");
+	printf("(q to quit): ");
+	input_is_good = (scanf("%ld", &num) == 1);
+	while (input_is_good)
+	{
+		sum = sum + num;
+		printf("Please enter next integer (q to quit): ");
+		input_is_good = (scanf("%ld", &num));
+	}
+	printf("Those integer sum to %ld.\n", sum);
+
+	return 0;
+}
