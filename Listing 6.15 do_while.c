00608d6023c3d3917c53af9b51c5758dc58f38cd
赵子豪 <zihao13969918264@hotmail.com>
2017-10-13 07:46:17 -0500

    Create Listing 6.15 do_while.c

---
+++ Listing 6.15 do_while.c
@@ -0,0 +1,21 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月11日
+*/
+/*	do_while.c -- exit condition loop	*/
+#include <stdio.h>
+int main(void)
+{
+	const int secret_code = 13;
+	int code_entered;
+
+	do
+	{
+		printf("To enter the triskaidekaphobia therapy club,\n");
+		printf("please enter the secret code number: ");
+		scanf("%d", &code_entered);
+	}while (code_entered != secret_code);
+	printf("Configuration! You are cured!\n");
+
+	return 0;
+}
