e23a720296312a194d648dda3ba66ddc5a963aa6
赵子豪 <zihao13969918264@hotmail.com>
2017-10-13 07:29:01 -0500

    Create Listing 6.12 for_down.c

---
+++ Listing 6.12 for_down.c
@@ -0,0 +1,16 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月10日
+*/
+/*	for_down.c */
+#include <stdio.h>
+int main(void)
+{
+	int secs;
+
+	for (secs = 5; secs > 0; secs--)
+		printf("%d seconds!\n", secs);
+	printf("We have ignition!\n");
+
+	return 0;
+}
