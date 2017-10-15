5dc08ac3caf0e47009e1d6c0a6332cbcb84dda3e
赵子豪 <13969918264@163.com>
2017-10-15 06:21:46 -0500

    Update Exercise 6.9 difference2.c

--- Exercise 6.9 difference2.c
+++ Exercise 6.9 difference2.c
@@ -5,7 +5,7 @@
 /*  difference2.c -- using a function   */
 #include <stdio.h>
 
-void different(float m, float n);
+double different(float m, float n);
 int main(void)
 {
     float number_one, number_two;
@@ -13,7 +13,9 @@ int main(void)
     printf("Enter two float number:\n");
     while (scanf("%f", &number_one) && scanf("%f", &number_two))
     {
-        different(number_one, number_two);
+        printf("You enter %f and %f.\n", number_one, number_two);
+        printf("The value of their difference divided by their product is %lf.\n", different(number_one, number_two));
+        printf("Please enter two float-point number:\n");
     }
     printf("You enter one nonnumberic at least! The program is quit.\n");
 
@@ -22,10 +24,9 @@ int main(void)
 
 
 
-void different(float m , float n)
+double different(float m , float n)
 {
-        double difference = m * n / (m - n);
-        printf("You enter %f and %f.\n", m, n);
-        printf("The value of their difference divided by their product is %lf.\n", difference);
-        printf("Please enter two float-point number:\n");
+    double difference = m * n / (m - n);
+
+    return difference;
 }
