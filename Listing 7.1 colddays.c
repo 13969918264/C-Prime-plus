5ee5f9702eddf15902fbee6520af842c4bb99f0b
赵子豪 <13969918264@163.com>
2017-10-18 18:30:22 +0800

    Create Listing 7.1 colddays.c

---
+++ Listing 7.1 colddays.c
@@ -0,0 +1,33 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月18日
+*/
+//	colddays.c -- finds percentage of days below freezing
+#include <stdio.h>
+int main(void)
+{
+	const int FREEZING = 0;
+	float temperature;
+	int cold_days = 0;
+	int all_days = 0;
+
+	printf("Enter the list of daily low temperaturel.\n");
+	printf("Use Celsius, and enter q to quit.\n");
+	while (scanf("%f", &temperature) == 1)
+	{
+		all_days++;
+		if (temperature < FREEZING)
+			cold_days++;
+	}
+	if (all_days != 0)
+		printf("%d days total: %.1f%% were below freezing.\n",
+				all_days, 100.0 * (float)cold_days / all_days);
+/*
+	else
+		printf("No data entered!\n");
+*/
+	if (all_days == 0)
+		printf("No data entered!\n");
+
+	return 0;
+}
