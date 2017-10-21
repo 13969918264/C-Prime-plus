298cbbf52d558344f124271bdbb631dfb76df208
赵子豪 <13969918264@163.com>
2017-10-21 04:14:07 -0500

    Create Listing 7.11 animals.c

---
+++ Listing 7.11 animals.c
@@ -0,0 +1,52 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年10月21日
+*/
+/*  animals.c -- uses a swithc statement    */
+#include <stdio.h>
+#include <ctype.h>
+int main(void)
+{
+    char ch;
+
+    printf("Give me a letter of the alphabet. and I will gibe ");
+    printf("an animal name\nbegining with taht letter.\n");
+    printf("Please typt in a letter; type # to end my act.\n");
+    while ((ch = getchar()) != '#')
+    {
+        if ('\n' == ch)
+            continue;
+        if (islower(ch))           /*  lowercase only  */
+            switch(ch)
+            {
+                case 'a' :
+                    printf("argali, a wild sheep of Asia\n");
+                    break;
+                case 'b' :
+                    printf("babirusa, a wild pig of Malay\n");
+                    break;
+                case 'c' :
+                    printf("coati, racoonlike mammal\n");
+                    break;
+                case 'd' :
+                    printf("desman, aquatic, milelike critter\n");
+                    break;
+                case 'e' :
+                    printf("echidna, the spiny anteater\n");
+                    break;
+                case 'f' :
+                    printf("fisher, brownish marten\n");
+                    break;
+                default :
+                    printf("That's a stumper!\n");
+            }           /*  end of switch   */
+        else
+            printf("I recognize only lowercase letters.\n");
+        while (getchar() != '\n')
+            continue;           /*  skip rest of input line */
+        printf("Please type another letter of a #.\n");
+    }           /*  while loop end  */
+    printf("Bye!\n");
+
+    return 0;
+}
