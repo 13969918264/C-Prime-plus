34b68717c8c3bc9ef853f38d108d19ea712b0e0d
赵子豪 <13969918264@163.com>
2017-10-22 04:09:35 -0500

    Revert "Optimization of code."

--- Exercise 6.10 sum_of_squares.c
+++ Exercise 6.10 sum_of_squares.c
@@ -8,8 +8,7 @@ int main(void)
 {
     int upper_limit, lower_limit;
     long int sum;
-    long int ans1;
-    long int ans2;
+    int index;
 
     printf("Please enter two integer.\n");
     printf("if you enter nonnumberic or lower is equal or greater than upper limit. the program will be over.\n");
@@ -17,10 +16,10 @@ int main(void)
     scanf("%d %d", &lower_limit, &upper_limit);
     while (lower_limit < upper_limit)
     {
-        ans1=upper_limit*(upper_limit+1)*(2*upper_limit+1)/6;
-        ans2=(lower_limit-1)*lower_limit*(2*lower_limit-1)/6;
-        sum=ans1-ans2;
-        
+        for (index = lower_limit, sum = 0; index <= upper_limit; index++)
+        {
+            sum += index * index;
+        }
         printf("The sums of the squares from %ld to %ld is %ld.\n",
                 lower_limit * lower_limit, upper_limit * upper_limit, sum);
         printf("Enter the next set of limits: ");
