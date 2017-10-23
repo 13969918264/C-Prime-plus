2e0c9b54bb8014cad8b9eb5de0b541d8e2d3d0a0
赵子豪 <13969918264@163.com>
2017-10-23 12:03:53 +0800

    Update Exercise 7.2 count_print.c

--- Exercise 7.2 count_print.c
+++ Exercise 7.2 count_print.c
@@ -5,26 +5,33 @@
 /*	count_print.c	*/
 /*	read input until encountering # to stop. print character and its decimal code	*/
 /*	there is a question is that when i enter the 'enter', the program starts without enter '#'	*/
+//	update: now i know the reason.
 #include <stdio.h>
 #define SIZE 8
 #define MAX 250
 int main(void)
 {
-	char ch;
 	int count = 0;
 	int index = 0;
 	char array[MAX];
+	char ch;
 
 	printf("Enter a line and # to quit \n");
-	while ((array[index++] = getchar()) != '#')
-		continue;
-	for ( ; count <= index; count++)
+	while ((ch = getchar()) != '#')
+	{
+	//	while语句中不能同时输出，否则不能换行输入，换行键立即执行。
+		if (ch == '\n')
+			continue;
+		else
+			array[index++] = ch;
+	}
+	for ( ; count < index; count++)
 	{
 		if ((count % SIZE) ==0 && count != 0)
 			printf("\n");
-		printf("%c %d\t", array[count], array[count]);
+		printf("%c %d\t", array[count], array[count]);			//	不能用else包括，否则每行只能输出7个
 	}
-	printf("\n program is over.\n");
+	printf("\nprogram is over.\n");
 
 	return 0;
 }
