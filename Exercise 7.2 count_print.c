956eda6c825f0d586ae2b8945c2f48511fe7880e
赵子豪 <13969918264@163.com>
2017-10-23 11:30:36 +0800

    Update Exercise 7.2 count_print.c

--- Exercise 7.2 count_print.c
+++ Exercise 7.2 count_print.c
@@ -7,19 +7,22 @@
 /*	there is a question is that when i enter the 'enter', the program starts without enter '#'	*/
 #include <stdio.h>
 #define SIZE 8
+#define MAX 250
 int main(void)
 {
 	char ch;
 	int count = 0;
+	int index = 0;
+	char array[MAX];
 
 	printf("Enter a line and # to quit \n");
-	while ((ch = getchar()) != '#')
+	while ((array[index++] = getchar()) != '#')
+		continue;
+	for ( ; count <= index; count++)
 	{
 		if ((count % SIZE) ==0 && count != 0)
 			printf("\n");
-		else
-			printf("%c %d\t", ch, ch);
-		count++;
+		printf("%c %d\t", array[count], array[count]);
 	}
 	printf("\n program is over.\n");
 
