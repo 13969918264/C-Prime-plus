a78675f02010b8c3bcce4735a94cc415bc154c73
赵子豪 <13969918264@163.com>
2017-10-25 03:47:23 -0500

    Update Exercise 7.9 prime.c

--- Exercise 7.9 prime.c
+++ Exercise 7.9 prime.c
@@ -11,6 +11,50 @@ int main(void)
 	int number;
 	int index;
 	int prime;
+	int flag = 0;
 
-	printf("Please enter a positive integer.\n");
+	printf("Please enter a positive integerfrom 1 to 10000.\n");
+	while (scanf("%d", &number))
+	{
+        if (number > MAX || number < MIN)
+        {
+            printf("The number you entered is too big or too small or too big\nPlease enter the right number\n");
+            continue;
+        }
+        else if (number == MIN)
+        {
+            printf("You enter %d.\nIt's no sense.\n", number);
+            printf("Enter the next number and q to quit.\n");
+            continue;
+        }
+        printf("Primes are (except 1) :\n");
+        for (prime = 2; prime <= number; prime++)
+        {
+            if (prime <= 3)
+                printf("%d\n", prime);
+            else
+            {
+                for (index = 2; index * index < prime; index++)
+                {
+                    if (prime % index != 0)
+                    {
+                        flag = 1;           //  目前为止不可除尽
+                        continue;
+                    }
+                    else
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+            if (flag == 1)
+                printf("%d.\n", prime);
+            }
+
+        }
+        printf("Enter the next number and q to quit.\n");
+    }
+    printf("The program is over.\n");
+
+    return 0;
 }
