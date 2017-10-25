72fb98e2e2de875898056f1a68d6c45de8c944d8
赵子豪 <13969918264@163.com>
2017-10-25 11:34:34 +0800

    Create Exercise 7.9 prime.c

---
+++ Exercise 7.9 prime.c
@@ -0,0 +1,16 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月25日
+*/
+/* prime.c -- enter a positive integer and print all the primes under the number given	*/
+#include <stdio.h>
+#define MAX 10000
+#define MIN 1
+int main(void)
+{
+	int number;
+	int index;
+	int prime;
+
+	printf("Please enter a positive integer.\n");
+}
