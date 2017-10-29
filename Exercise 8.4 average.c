a240fc0634f0ee502feb6dfec39b1c9df10b4fb7
赵子豪 <13969918264@163.com>
2017-10-29 08:57:36 -0500

    Create Exercise 8.4 average.c

---
+++ Exercise 8.4 average.c
@@ -0,0 +1,28 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年10月29日
+*/
+/*  average.c -- account the average number of letters per word */
+#include <stdio.h>
+#include <ctype.h>
+int main(void)
+{
+    int account_letters;
+    int account_words;
+    char ch;
+
+    printf("Enter words and it will report the average number of letters per word when it stop.\n");
+    for (account_letters = account_words = 0; (ch = getchar()) != EOF; )
+    {
+        if (isalpha(ch))
+            account_letters++;
+        else if (ch == '\0' || ch == '\n')
+            account_words++;
+    }
+    printf("\nThe number of letters is %d, "
+           "the number of word is %d, "
+           "the average number of letters is %d.\n",
+           account_letters, account_words, account_letters / account_words);
+
+    return 0;
+}
