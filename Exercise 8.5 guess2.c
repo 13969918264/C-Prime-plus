48ef572f9ab25d14747f1e0bbf3efaad55ea2e69
赵子豪 <13969918264@163.com>
2017-10-30 10:40:21 +0800

    Create Exercise 8.5 guess2.c

---
+++ Exercise 8.5 guess2.c
@@ -0,0 +1,49 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月30日
+*/
+/*	guess2.c -- an efficient and right number-guesser	*/
+#include <stdio.h>
+#define MAX 100
+#define MIN 0
+#define RATE 2
+int main(void)
+{
+	int max = MAX;
+	int min = MIN;
+	int middle = (max + min) / RATE;
+	char ch;
+	int guess = middle;
+
+	printf("Pick an integer form 0 to 100, I will try to guess "
+		   "it.\nRespond with high if the guess is grater than it.\n"
+		   "Respond with low if the guess is less than it.\n"
+		   "Respond correct if the guess is right.\n");
+	printf("Is your number is %d.\n", guess);
+	while ((ch = getchar()) != 'c')
+	{
+		if (ch == 'h')
+		{
+			printf("I guess it highly.\n");
+			max = middle;
+			middle = (min + max) / RATE;
+			guess = middle;
+			printf("well, your number is %d.\n", guess);
+		}
+		else if (ch == 'l')
+		{
+			printf("I guess it lower.\n");
+			min = middle;
+			middle = (min + max) / RATE;
+			guess = middle;
+			printf("well, your number is %d.\n", guess);
+		}
+		else
+			printf("Please enter the choice given.\n");
+		while (getchar() != '\n')
+			continue;
+	}
+	printf("I know I could do it.\n");
+
+	return 0;
+}
