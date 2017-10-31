85d5e5a5eb856510d92854f9f93314b562245d46
赵子豪 <13969918264@163.com>
2017-10-31 11:09:50 +0800

    Create Listing 9.2 lethead2.c

---
+++ Listing 9.2 lethead2.c
@@ -0,0 +1,44 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年10月31日
+*/
+/*	lethead2.c	*/
+#include <stdio.h>
+#include <string.h>
+#define NAME "GIGATHINK, INC."
+#define ADDRESS "101 Megabuck Plaza"
+#define PLACE "Megapolis, CA 94904"
+#define WIDTH 40
+#define SPACE ' '
+
+void show_n_char(char ch, int num);
+
+int main(void)
+{
+	int spaces;
+
+	show_n_char('*', WIDTH); 			/*	using constants as arguments	*/
+	putchar('\n');
+	show_n_char(SPACE, 12);			/*	using constants as arguments	*/
+	printf("%s\n", NAME);
+	spaces = (WIDTH - strlen(ADDRESS)) / 2;
+			/*	Let the program calculate	*/
+			/*	how many space to skip	*/
+	show_n_char(SPACE, spaces);			/*	use a varible as argument	*/
+	printf("%s\n", ADDRESS);
+	show_n_char(SPACE, (WIDTH - strlen(PLACE)) / 2);			/*	an expression as argument*/
+	printf("%s\n", PLACE);
+	show_n_char('*', WIDTH);
+	putchar('\n');
+
+	return 0;
+}
+
+/*	show_n_char() definition	*/
+void show_n_char(char ch, int num)
+{
+	int count;
+
+	for (count = 1; count <= num; count++)
+		putchar(ch);
+}
