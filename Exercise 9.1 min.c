1e6e5aab623bee45f860d742eaf7e22ed4829b3a
赵子豪 <13969918264@163.com>
2017-11-02 08:15:06 -0500

    Create Exercise 9.1 min.c

---
+++ Exercise 9.1 min.c
@@ -0,0 +1,9 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date; 2017年11月02日
+*/
+/*  min.c -- return the smaller number  */
+double min(double x, double y)
+{
+    return x <= y ? x : y;          //  you can use if and else as well.
+}
