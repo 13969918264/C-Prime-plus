cb828f9351ae8f5fb8d990aa8d1d4eb696d5de46
赵子豪 <13969918264@163.com>
2017-11-03 06:48:18 -0500

    Update Exercise 9.4 reverse.c

--- Exercise 9.4 reverse.c
+++ Exercise 9.4 reverse.c
@@ -2,19 +2,8 @@
     Name: 赵子豪(ZiHao Zhao)
     Date: 2017年11月03日
 */
-/*  harmonic.c -- reverse the numbers by using a function   */
-#include <stdio.h>
-#include "harmonic.h"
-
-int main(void)
+/*  reverse.c -- reverse the number */
+double reverse(double m, double n)
 {
-    double first, second;
-    double result;
-
-    printf("Enter 2 number.\n");
-    scanf("%lf %lf", &first, &second);
-    result = reverse(first, second);
-    printf("%lf\n", result);
-
-    return 0;
+    return -(-m - n) / 2;
 }
