8ed5a01d5ff00708ad28e30c6044d51f93a690f6
赵子豪 <13969918264@163.com>
2017-11-05 07:30:04 -0600

    Create Exercise 9.10 convert.c

---
+++ Exercise 9.10 convert.c
@@ -0,0 +1,23 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月05日
+*/
+/*  convert.c -- represent a number by another base */
+#include <stdio.h>
+#include "convert.h"
+
+int main(void)
+{
+    int number, base, result;
+
+    printf("Enter a number based-10:\n");
+    scanf("%d", &number);
+    printf("Enter the new base:\n");
+    scanf("%d", &base);
+    printf("You entered %d based-10.\n ", number);
+    printf("equivalent base-%d.\n", base);
+    to_base_n(number, base);
+    putchar('\n');
+
+    return 0;
+}
