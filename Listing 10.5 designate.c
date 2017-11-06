62114bd85e30bdb316ccab886390ec6c542c8861
赵子豪 <13969918264@163.com>
2017-11-06 02:24:49 -0600

    Create Listing 10.5 designate.c

---
+++ Listing 10.5 designate.c
@@ -0,0 +1,17 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+ऀ	Date: 2017年11月06日
+*/
+/*  designate.c -- use designate initializers   */
+#include <stdio.h>
+#define MONTHS 12
+int main(void)
+{
+    int days[MONTHS] = {31, 28, [4] = 31, 30, 31, [1] = 29};
+    int i;
+
+    for (i = 0; i < MONTHS; i++)
+        printf("%2d  %d\n", i + 1, days[i]);
+
+    return 0;
+}
