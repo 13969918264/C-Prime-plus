72a14c9fd56062d011aeb511dcb7dd75f412edc4
赵子豪 <13969918264@163.com>
2017-11-06 02:48:24 -0600

    Create Listing 10.6 bounds.c

---
+++ Listing 10.6 bounds.c
@@ -0,0 +1,28 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+ऀ	Date: 2017年11月06日
+*/
+//  bounds.c -- exceed the bounds of an array
+#include <stdio.h>
+#define SIZE 4
+int main(void)
+{
+    int value1 = 44;
+    int arr[SIZE];
+    int value2 = 88;
+    int i;
+
+    printf("value1 = %d, value2 = %d\n", value1, value2);
+    for (i = -1; i <= SIZE; i++)
+        arr[i] = 2 * i + 1;
+
+    for (i = -1; i < 7; i++)
+        printf("%2d  %d\n", arr[i]);
+    printf("value1 = %d, value2 = %d\n", value1, value2);           //  in mac os x, little endian.
+    printf("address of arr[-1]: %p\n", &arr[-1]);
+    printf("address of arr[4]:  %p\n", &arr[4]);
+    printf("address of value1:  %p\n", &value1);
+    printf("address of value2:  %p\n", &value2);
+
+    return 0;
+}
