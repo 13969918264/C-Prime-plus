369281fce958f59d5462a363596e71f128bfb65a
赵子豪 <13969918264@163.com>
2017-11-09 07:56:45 -0600

    Create Exercise 10.2 copy_ptrs.c

---
+++ Exercise 10.2 copy_ptrs.c
@@ -0,0 +1,12 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月09日
+*/
+/*  copy_ptrs.c -- the third way to copy an array with the pointer of the last element of the source array  */
+copy_ptrs(double target[], double source[], double * ptrs)
+{
+    double * ptr = source;
+    int i;
+    for (i = 0; ptr < ptrs; ptr++)
+        target[i++] = *ptr;
+}
