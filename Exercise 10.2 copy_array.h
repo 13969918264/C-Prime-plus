cae0ff5b41ef3002db1660a19f5bc9666457b1d5
赵子豪 <13969918264@163.com>
2017-11-09 07:57:31 -0600

    Create Exercise 10.2 copy_array.h

---
+++ Exercise 10.2 copy_array.h
@@ -0,0 +1,9 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月09日
+*/
+/* copy_array.h */
+#define N 5
+void copy_arr(double [], double [], int);
+void copy_ptr(double *, double *, int);
+void copy_ptrs(double [], double [], double *);
