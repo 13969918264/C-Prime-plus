774f25fdd99a718df915b346b39c383efa8eebf7
赵子豪 <13969918264@163.com>
2017-11-14 06:02:33 -0600

    Create Exercise 10.7 copy2.h

---
+++ Exercise 10.7 copy2.h
@@ -0,0 +1,11 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月14日
+*/
+/*  copy2.h */
+#define N 5
+#define COLS 5
+#define ROWS 2
+
+void print_array(double [][COLS], int);         //  another syntax : double (*arr)[COLS]
+void copy_arr(double [], double [], int);
