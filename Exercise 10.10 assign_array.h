fa39bdeb9932e262bb2024e33bdcd64c0a7d422e
赵子豪 <13969918264@163.com>
2017-11-15 07:12:38 -0600

    Create Exercise 10.10 assign_array.h

---
+++ Exercise 10.10 assign_array.h
@@ -0,0 +1,9 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月15日
+*/
+/*  assign_array.h  */
+#define N 4
+
+void assign_sum(int *, int *, int *, int);
+void print4_array(int *, int *, int *, int);
