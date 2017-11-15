be186f55f1fef507060dabe0842185b06fa577ee
赵子豪 <13969918264@163.com>
2017-11-15 11:50:37 +0800

    Create Exercise 10.9 VLA_copy.h

---
+++ Exercise 10.9 VLA_copy.h
@@ -0,0 +1,10 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年11月15日
+*/
+/*	VLA_copy.h	*/
+#define N 3
+#define M 5
+void print3_array(double [][M], int);
+void copy_VLA(int, int, double [*][*], double [*][*]);			//	omit name
+void print_VLA(int, int, double [*][*]);
