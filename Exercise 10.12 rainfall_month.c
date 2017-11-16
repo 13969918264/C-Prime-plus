36a3a354cd3884a422d5cbeb1e270957c852a128
赵子豪 <13969918264@163.com>
2017-11-16 05:50:50 -0600

    Update Exercise 10.12 rainfall_month.c

--- Exercise 10.12 rainfall_month.c
+++ Exercise 10.12 rainfall_month.c
@@ -3,19 +3,20 @@
     Date: 2017年11月16日
 */
 /*  rainfall_month.c -- the rainfall of each month  */
+#include <stdio.h>
 #define MONTHS 12
-void rainfall_month(float rainfall[][MONTHS], int years)
+void rainfall_month(const float rain[][MONTHS], int years)
 {
     int year, month;
     float subtot;
 
     printf("MONTHLY AVERAGE:\n\n");
-    printf(" Jan  Feb  Mar  Apr  May  Jun  Jul  Aug  Sep  Oct  Nov  Dec\n");
-    for (month = 0; month < MONTHS, month++)
+    printf(" Jan\tFeb\tMar\tApr\tMay\tJun\tJul\tAug\tSep\tOct\tNov\tDec\n");
+    for (month = 0; month < MONTHS; month++)
     {   //          for each month , sum rainfall over years
         for (year = 0, subtot = 0; year < years; year++)
             subtot += rain[year][month];
-        printf("%4.1f  ", subtot / years);
+        printf("%4.1f\t", subtot / years);
     }
     printf("\n");
 }
