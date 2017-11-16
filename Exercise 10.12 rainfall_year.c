ec49d468700d7577e3c730036cd438fec69e5536
赵子豪 <13969918264@163.com>
2017-11-16 05:51:19 -0600

    Update Exercise 10.12 rainfall_year.c

--- Exercise 10.12 rainfall_year.c
+++ Exercise 10.12 rainfall_year.c
@@ -5,7 +5,7 @@
 /*  rainfall_year.c -- print the rainfall of each year  */
 #include <stdio.h>
 #define MONTHS 12
-void rainfall(const float rain[][MONTHS], int years)
+void rainfall_year(const float rain[][MONTHS], int years)
 {
     int year, month;
     float subtot, total;
@@ -13,12 +13,12 @@ void rainfall(const float rain[][MONTHS], int years)
     printf("YEAR    RAINFALL (INCHES)\n");
     for (year = 0, total = 0; year < years; year++)
     {   //  for each year, sum rainfall for each year
-        for (month = 0, subtotal = 0; month < months; month++)
-            subtotal += rain[year][month];
+        for (month = 0, subtot = 0; month < MONTHS; month++)
+            subtot += rain[year][month];
         printf("%5d     %15.1f\n", 2010 + year, subtot);
         total += subtot;            //  total for all years
     }
-    printf("\nThe total rainfall is %f.\n",
+    printf("\nThe total rainfall is %f.\n"
            "The yearly average is %.1f inches\n\n",
            total, total / years);
 }
