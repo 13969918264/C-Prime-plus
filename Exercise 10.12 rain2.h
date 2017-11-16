f71bcb2835602b127fe59825745854a6d4db6c10
赵子豪 <13969918264@163.com>
2017-11-16 05:15:51 -0600

    Create Exercise 10.12 rain2.h

---
+++ Exercise 10.12 rain2.h
@@ -0,0 +1,10 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月16日
+*/
+/*  rain2.h  */
+#define MONTHS 12           //  number of months in a year
+#define YEARS 5         //  number of years of data
+
+void rainfall_year(const float [][MONTHS], int);
+void rainfall_month(const floar [][MONTHS], int);
