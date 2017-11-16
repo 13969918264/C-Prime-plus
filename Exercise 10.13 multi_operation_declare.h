ac1f56b9abfe389766dc2fcdac31c59f8687df68
赵子豪 <13969918264@163.com>
2017-11-16 07:08:08 -0600

    Create Exercise 10.13 multi_operation_declare.h

---
+++ Exercise 10.13 multi_operation_declare.h
@@ -0,0 +1,9 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月16日
+*/
+/*  multi_operation_declare.h   */
+void store(double [][M], int);
+double average_each(double *, int);
+double average_all(double [][M], int);
+double largest(double [][M], int);
