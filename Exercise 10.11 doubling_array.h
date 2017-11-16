3cb9865f27b492cfbdaeb89e7210179a1fdb86b6
赵子豪 <13969918264@163.com>
2017-11-16 04:30:15 -0600

    Create Exercise 10.11 doubling_array.h

---
+++ Exercise 10.11 doubling_array.h
@@ -0,0 +1,9 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月15日
+*/
+/*  doubling_array.h    */
+#define N 3
+#define M 5
+void print5_array(int [][M], int);          //  declaration should be the style given.
+void doubling(int (*arr)[M], int);          //  declaration and difinition of a function with a pointer can be in difference style.
