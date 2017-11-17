992863d4e95eff66868371da70f46ac971e2920b
赵子豪 <13969918264@163.com>
2017-11-17 10:22:43 +0800

    Create Exercise 10.14 multi2_operation.h

---
+++ Exercise 10.14 multi2_operation.h
@@ -0,0 +1,9 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年11月17日
+*/
+/*	multi2_operation.h	*/
+void store2(int, int, double [*][*]);
+double average2_each(int, double *);
+double average2_all(int, int, double [*][*]);
+double largest(int, int, double [*][*]);
