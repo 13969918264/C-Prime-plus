38821c47866a75fbe3bee2d9c921a80a07c7cf9f
赵子豪 <13969918264@163.com>
2017-11-17 11:49:13 +0800

    Update Listing 11.3 addresses.c

--- Listing 11.3 addresses.c
+++ Listing 11.3 addresses.c
@@ -11,5 +11,10 @@ int main(void)
 	char ar[] = MSG;
 	const char *pt = MSG;
 	printf("address of \"I'm special\": %p \n", "I'm special");
-	printf("			  address ar: %p\n", ar);
-	}
+	printf("              address ar: %p\n", ar);
+	printf("              address pt: %p\n", pt);
+	printf("         address of  MSG: %p\n", MSG);
+	printf("address of \"I'm special\": %p \n", "I'm special");
+
+	return 0;
+}
