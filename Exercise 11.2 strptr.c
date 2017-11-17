29e7a1390e7e6ed859462b88239d58ff520b4765
赵子豪 <13969918264@163.com>
2017-11-17 10:55:53 +0800

    Create Exercise 11.2 strptr.c

---
+++ Exercise 11.2 strptr.c
@@ -0,0 +1,12 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年11月17日
+*/
+/*	strptr.c -- strings as pointers	*/
+#include <stdio.h>
+int main(void)
+{
+	printf("%s, %p, %c\n", "We", "are", *"space farers");
+
+	return 0;
+}
