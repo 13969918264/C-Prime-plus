2167373b7f9d8bfb5b0f29368e27f31ad4766575
赵子豪 <13969918264@163.com>
2017-11-19 15:36:28 +0800

    Create Listing 11.13 nono.c

---
+++ Listing 11.13 nono.c
@@ -0,0 +1,16 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月19日
+*/
+/*  nono.c -- no!   */
+#include <stdio.h>
+int main(void)
+{
+    char side_a[] = "Side A";
+    char dont[] = {'W', 'O', 'W', '|'};
+    char side_b[] = "Side B";
+
+    puts(dont);         /*  dont is not a string    */
+
+    return 0;
+}
