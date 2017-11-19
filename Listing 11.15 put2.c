d25bceef16bc89c63400defe3f2b36dbbfcd64e9
赵子豪 <13969918264@163.com>
2017-11-19 18:08:36 +0800

    Update Listing 11.15 put2.c

--- Listing 11.15 put2.c
+++ Listing 11.15 put2.c
@@ -9,7 +9,12 @@ int put2(const char * string)
     int count = 0;
     while (*string)         /*  common idiom    */
     {
-        putchar(*string++);
+        putchar(*string++);         //  The type of the parameter of putchar() is int
+        //  so the expression must produce a integer.
+        //  As we know,  ++ is higher precedence than *, so perform ++ first.
+        //  But ++ is postfixed here, that means the varible should evaluate first, and then increments.
+        //  So the varible 'string' evaluate with operator '*' as the parameter.
+        //  That's about what I think, and I don't if it is wrong.
         count++;
     }
     putchar('\n');          /*  newline not counted */
