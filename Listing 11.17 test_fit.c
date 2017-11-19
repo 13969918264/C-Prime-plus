a454b02fb68511078eaf838c2b873bab5a5c48a1
赵子豪 <13969918264@163.com>
2017-11-19 18:47:29 +0800

    Create Listing 11.17 test_fit.c

---
+++ Listing 11.17 test_fit.c
@@ -0,0 +1,28 @@
+/*
+    Name: 赵子豪(ZiHao Zhao)
+    Date: 2017年11月19日
+*/
+/*  test_fit.c -- try the string-shrinking function */
+#include <stdio.h>
+#include <string.h>         //  containing string functions prototypes
+void fit(char *, unsigned int);
+
+int main(void)
+{
+    char mesg[] = "Things should be as simple as possible,"
+    " but not simpler.";
+
+    puts(mesg);
+    fit(mesg, 38);
+    puts(mesg);
+    puts("Let's look at some more of the string.");
+    puts(mesg + 39);
+
+    return 0;
+}
+
+void fit(char * string, unsigned int size)
+{
+    if (strlen(string) > size)
+        string[size] = '\0';
+}
