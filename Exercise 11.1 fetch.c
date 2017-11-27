3211b843665225ba36c6de8907ac1c4d7d94d40d
赵子豪 <13969918264@163.com>
2017-11-27 10:22:19 +0800

    Create Exercise 11.1 fetch.c

---
+++ Exercise 11.1 fetch.c
@@ -0,0 +1,18 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年11月27日
+*/
+/*	fetch.c -- fetch the nest n character from input (including blanks, tools and newline)	*/
+#include <stdio.h>
+#include "fetch.h"
+
+int main(void)
+{
+	char str[MAX];
+	puts("Enter a line");
+	fetch_n(str, MAX);
+	puts("You enter (less the maximum)");
+	pus(str);
+
+	return 0;
+}
