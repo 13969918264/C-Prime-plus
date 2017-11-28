5f2917fc592bdabb9338ed979b2f284769324311
赵子豪 <13969918264@163.com>
2017-11-28 11:30:27 +0800

    Create Exercise 11.5 search_test.c

---
+++ Exercise 11.5 search_test.c
@@ -0,0 +1,28 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年11月28日
+*/
+/*	search_test.c	*/
+#include <stdio.h>
+#include "search_test.h"
+
+int main(void)
+{
+	int i = 0;
+	char ch;
+	char str[MAX];
+	puts("Enter a line as you like.");
+	while (i < MAX)
+	{	str[i] = getchar();
+		if (str[i] == '\n')
+			break;
+	}
+	puts("And now which alphabet do you search for?");
+	scanf("%c", &ch);
+	if (search(str, ch))
+		puts("Congratulation! You find it.");
+	else
+		puts("Oh, no, it is not in the statement.");
+
+	return 0;
+}
