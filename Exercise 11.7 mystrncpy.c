a2b0085b08024f2f6b19f9b0de4a04555631d7ec
赵子豪 <13969918264@163.com>
2017-11-30 10:40:22 +0800

    Create Exercise 11.7 mystrncpy.c

---
+++ Exercise 11.7 mystrncpy.c
@@ -0,0 +1,31 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年11月30日
+*/
+/*	mystrncpy.c	*/
+#include <string.h>
+
+char * mystrncpy(char * st1, char * st2, int n)
+{
+	char * ret_val
+	int i = 0;
+
+	ret_val = st1;
+	while (i < n && *st2++ != '\0')
+	{
+		i++;
+		if (sizeof(st1) > n || i < (sizeof(str1) / sizeof(int) - 1))
+		{
+			*st1++ = *st2++;
+			continue;
+		}
+		else
+		{
+			*st1 = '\0';
+			break;
+		}
+	}
+	*st1 = '\0';
+
+	return ret_val;
+}
