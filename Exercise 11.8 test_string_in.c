31c5431768305854e0e8f227bd981e5470ae6d7e
赵子豪 <13969918264@163.com>
2017-11-30 10:55:47 +0800

    Create Exercise 11.8 test_string_in.c

---
+++ Exercise 11.8 test_string_in.c
@@ -0,0 +1,44 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年11月30日
+*/
+/*	test_string_in.c	*/
+#include <stdio.h>
+#include <string.h>
+#include "test_string_in.h"
+
+int main(void)
+{
+	char str1[MAX], str2[MAX];
+	int i;
+	puts("Enter the first string:");
+	for (i = 0; i < MAX &&(str1[i] = getchar()) != '\n' && str[i] != '\0'; i++)
+		continue;
+	if (str1[i] == '\n')
+		str1[i] = '\0';
+	else
+	{
+		str1[MAX - 1] = '\0';
+		while (getchar() != '\n')
+			continue;
+	}
+	puts("Enter the second string.");
+	for (i = 0; i < MAX &&(str2[i] = getchar()) != '\n' && str[i] != '\0'; i++)
+		continue;
+	if (str2[i] == '\n')
+		str2[i] = '\0';
+	else
+	{
+		str2[MAX - 1] = '\0';
+		while (getchar() != '\n')
+			continue;
+	}
+	if (strlen(str1) < strlen(str2))
+		puts("The size of the second string is larger than the first.");
+	else if (string_in(str1, str2))
+		puts("Congratulation.");
+	else
+		puts("Bad luck.");
+
+	return 0;
+}
