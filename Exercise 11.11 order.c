417248e115f7841ffc7a268c613a7eac747b16e4
赵子豪 <13969918264@163.com>
2017-12-04 10:25:07 +0800

    Create Exercise 11.11 order.c

---
+++ Exercise 11.11 order.c
@@ -0,0 +1,19 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年12月04日
+*/
+/*	order.c -- sort the strings in ASCII collating sequence	*/
+#include <stdio.h>
+void order(char * st, int num)
+{
+	int i, j;
+	char * pt;
+	for (i = 0; i < num - 2; i++)}
+		for (j = 1; j < num - 1; j++)
+			if (strcmp(st[i], st[j]) > 0)
+			{
+				pt = st[i];
+				st[i] = st[j];
+				st[j] = pt;
+			}
+}
