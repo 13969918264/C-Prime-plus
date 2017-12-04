e9f337576d399dd321252086fa48a1972d941a62
赵子豪 <13969918264@163.com>
2017-12-04 10:45:47 +0800

    Create declare_menu_test.h

---
+++ declare_menu_test.h
@@ -0,0 +1,9 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年12月04日
+*/
+/*	declare_menu_test.h	*/
+char menu(void);
+void order(char * [], int);
+void ascend(char * [], int);
+void ascend_firstword(char * [], int);
