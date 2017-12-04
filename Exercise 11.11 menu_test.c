da9cd2f589814d026d9edbe9b4042cdb15ba67c8
赵子豪 <13969918264@163.com>
2017-12-04 10:12:33 +0800

    Create Exercise 11.11 menu_test.c

---
+++ Exercise 11.11 menu_test.c
@@ -0,0 +1,61 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年12月04日
+*/
+/* 	menu_test.c	*/
+#include <stdio.h>
+#include <string.h>
+#include "define_menu_test.h"
+#include "declare_menu_test.h"
+int main(void)
+{
+	int length;
+	int i, j;
+	char str[ROWS][COLS];
+	char (* p) [COLS];
+	char ch;
+	p = str;
+	puts("Enter as you like up to 10, or EOF to quit.");
+	for(i = 0; scanf("%s", *p) && i < ROWS; i++)
+	{
+		length = strlen(*pt);
+		while (length < COLS - 1)
+		{
+			*(*p + length) = '\0';
+			length++;
+		}
+		*(*pt + COLS - 1) = '\0';
+	}
+	j = (i < ROWS) ? i : ROWS;
+	while ((ch = menu(void) != 'e')
+	{
+		switch (ch)
+		{
+			case 'a' : 
+						for (i = 0; i < j; i++)
+							puts(*p + i);
+						break;
+			case 'b' :
+						order(str);
+						for (i = 0; i < j; i++)
+							puts(*p + i);
+						break;
+			case 'c' :
+						ascend(str);
+						for (i = 0; i < j; i++)
+							puts(*p + i);
+						break;
+			case 'd' :
+						ascend_firstword(str);
+						for (i = 0; i < j; i++)
+							puts(*p + i);
+						break;
+			default : 
+						puts("please enter the right choice.");
+		}
+	}
+	if (ch == 'e')
+		puts("The program is over. bye!");
+	
+	return 0;
+}
