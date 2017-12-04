e81fcb6921f66e7b4743b966c78f371324f7413c
赵子豪 <13969918264@163.com>
2017-12-04 11:18:22 +0800

    Create Exercise 11.13 echo.c

---
+++ Exercise 11.13 echo.c
@@ -0,0 +1,14 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年12月04日
+*/
+/*	echo.c -- echo the command-line argumens in reversed word order	*/
+int main(int argc, char * argv[])
+{
+	int i;
+	puts("Print the command-line arguments in reversed order.");
+	for (i = argc - 1; i > 0; i--)
+		puts(argv[i]);
+
+	return 0;
+}
