1ceb6ad412c97724c171416ce876da3d089dc51e
赵子豪 <13969918264@163.com>
2017-12-05 09:54:43 +0800

    Create Exercise 11.16 echo_choice.c

---
+++ Exercise 11.16 echo_choice.c
@@ -0,0 +1,21 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年12月05日
+*/
+/*	echo_choice.c	*/
+#include <stdio.h>
+#include "echo_choice.h"
+int main(int argc, char * argv[])
+{
+	char str[ROWS][COLS];
+	int i, j;
+	puts("Enter as you like.");
+	for (i = 0; i < ROWS; i++)
+		if (s_gets(str[i], COLS))
+			continue;
+		else
+			break;
+	echo(str, argv[1]);
+
+	return 0;
+}
