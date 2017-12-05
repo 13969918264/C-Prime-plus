74c9135c3095de1210e2a3dadc281ef6935e53f0
赵子豪 <13969918264@163.com>
2017-12-05 11:20:38 +0800

    Update Exercise 11.2 fetch2_n.c

--- Exercise 11.2 fetch2_n.c
+++ Exercise 11.2 fetch2_n.c
@@ -6,21 +6,18 @@
 #include <stdio.h>
 #include <string.h>
 
-char * fetch2_n(char * st, int n)
+void fetch2_n(char * st, int n)
 {
 /*	//	In this way, the program is process one line.
-	char * ret_val;
 	char * first;
 
-	ret_val = fgets(st, n, stdin);
+	fgets(st, n, stdin);
 	if (first = strchr(st, '\n'))
 		*first = '\0';
 */
 
-	char * ret_val;
 	int i = 0;
 
-	ret_val = st;
 	while (i < n && (*st = getchar()) != EOF)
 	{
 		st++;
@@ -28,5 +25,4 @@ char * fetch2_n(char * st, int n)
 	}
 	*st = '\0';
 
-	return ret_val;
 }
