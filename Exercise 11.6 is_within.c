c715491a4fee2c76e07c1bbc01b738a79a604617
赵子豪 <13969918264@163.com>
2017-12-07 12:56:52 +0800

    Update Exercise 11.6 is_within.c

--- Exercise 11.6 is_within.c
+++ Exercise 11.6 is_within.c
@@ -3,12 +3,14 @@
 	Date: 2017年11月29日
 */
 /*	is_within.c	-- decide if a character is within a string	*/
-bool is_within(char * st, char ch)
+int is_within(char * st, char ch)
 {
-	bool flag = false;
+	int flag = 0;
 
-	while (*st && *st++ == ch)
-		flag = true;
+	while (*st && *st++ != ch)
+		continue;
+	if (*--st == ch)
+		flag = 1;
 
 	return flag;
 }
