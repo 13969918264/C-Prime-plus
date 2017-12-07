1282c152cc703c2fbf4fb3739c3ea2d56c94fb2d
赵子豪 <13969918264@163.com>
2017-12-07 15:29:55 +0800

    Update Exercise 11.7 mystrncpy.c

--- Exercise 11.7 mystrncpy.c
+++ Exercise 11.7 mystrncpy.c
@@ -4,28 +4,16 @@
 */
 /*	mystrncpy.c	*/
 #include <string.h>
-
 char * mystrncpy(char * st1, const char * st2, int n)
 {
-	char * ret_val
 	int i = 0;
 
-	ret_val = st1;
-	while (i < n && *st2++ != '\0')
+	while (i < n)
 	{
+		st1[i] = st2[i];
 		i++;
-		if (sizeof(st1) > n || i < (sizeof(str1) / sizeof(int) - 1))
-		{
-			*st1++ = *st2++;
-			continue;
-		}
-		else
-		{
-			*st1 = '\0';
-			break;
-		}
 	}
-	*st1 = '\0';
+	st1[i] = '\0';
 
-	return ret_val;
+	return st1;
 }
