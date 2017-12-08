a769b8ba9a55aa3c46930e390c63f0e944e704bb
赵子豪 <13969918264@163.com>
2017-12-08 11:54:05 +0800

    Update Exercise 11.8 string_in.c

--- Exercise 11.8 string_in.c
+++ Exercise 11.8 string_in.c
@@ -6,36 +6,40 @@
 				return the address in the first paremters	*/
 #include <string.h>
 #include <stddef.h>
+#include <stdio.h>
 char * string_in(const char * st1, const char * st2)
 {
 	char * pt = NULL;
-	int i = 0, j = 0;
-	int flag = 0;
+	int i = 0, j;
+	int begin = i + 1;
 
-	for (i; i <= (strlen(st1) - strlen(st2)); i++)			//	st1
+	printf("%d\n", strlen(st1) - strlen(st2));
+	for (j = 0, i; i <= strlen(st1) && j <= strlen(st2); )
 	{
-		for (j; j <= strlen(st2); j++)			//	st2
+		if (*(st1 + i) == *(st2 + j))
 		{
-			if (*(st1 + i) == *(st2 + j))
+			if (j = 0)
 			{
-				if (j = 0)
-					pt = st1 + i;
-				flag = 1;
-				break;
+				pt = st1[i];
+				begin = i + 1;			//	pointed the index after the first address compatible.
+				//	if it doesnot fit finally, start the next cycle from here.
+				//	compare its value with the first value of the first character
+				//	of the second string.
 			}
-			else
-			{
-				pt = NULL;
-				flag = 0;
-			}
-		}
-		if (flag)			//	st1 and st2 change currently and continually
+			i++;
 			j++;
+		}
 		else
+		{
+			pt = NULL;
 			j = 0;
+			i = begin;			//	assign i as the new beginner.
+			if (begin > (strlen(st1) - strlen(st2)))
+				break;			//	when the rest of the string1 is less the string1, quit.
+			else
+				begin++;
+		}
 	}
 
 	return pt;
 }
-
-
