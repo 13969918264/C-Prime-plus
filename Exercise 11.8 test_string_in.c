147a4d3452b18b0d5ab95aa76d3dae31cfe11c5b
赵子豪 <13969918264@163.com>
2017-12-08 11:55:21 +0800

    Update Exercise 11.8 test_string_in.c

--- Exercise 11.8 test_string_in.c
+++ Exercise 11.8 test_string_in.c
@@ -5,38 +5,58 @@
 /*	test_string_in.c	*/
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 #include "test_string_in.h"
 
 int main(void)
 {
 	char str1[MAX], str2[MAX];
 	int i;
-	puts("Enter the first string:");
-	for (i = 0; i < MAX &&(str1[i] = getchar()) != '\n' && str1[i] != '\0'; i++)
-		continue;
-	if (str1[i] == '\n')
-		str1[i] = '\0';
-	else
+	int flag = 1;			//	if the length of then first string is less than the second one
+	char * begin;
+
+	while (flag == 1)
 	{
-		str1[MAX - 1] = '\0';
-		while (getchar() != '\n')
+		puts("Enter the first string:");
+		for (i = 0; i < MAX &&(str1[i] = getchar()) != '\n' && str1[i] != '\0'; i++)
 			continue;
+		if (str1[i] == '\n')
+			str1[i] = '\0';
+		else
+		{
+			str1[MAX - 1] = '\0';
+			while (getchar() != '\n')
+				continue;
+		}
+		puts("Enter the second string.");
+		for (i = 0; i < MAX &&(str2[i] = getchar()) != '\n' && str2[i] != '\0'; i++)
+			continue;
+		if (str2[i] == '\n')
+			str2[i] = '\0';
+		else
+		{
+			str2[MAX - 1] = '\0';
+			while (getchar() != '\n')
+				continue;
+		}
+		if (strlen(str1) < strlen(str2))
+		{
+			puts("The size of the second string is larger than the first.");
+			flag = 1;
+		}
+		else
+			flag = 0;
 	}
-	puts("Enter the second string.");
-	for (i = 0; i < MAX &&(str2[i] = getchar()) != '\n' && str2[i] != '\0'; i++)
-		continue;
-	if (str2[i] == '\n')
-		str2[i] = '\0';
-	else
+	puts("The first string you entered is:");
+	puts(str1);
+	puts("The second string you entered is:");
+	puts(str2);
+	begin = string_in(str1, str2);
+	if (begin)
 	{
-		str2[MAX - 1] = '\0';
-		while (getchar() != '\n')
-			continue;
-	}
-	if (strlen(str1) < strlen(str2))
-		puts("The size of the second string is larger than the first.");
-	else if (string_in(str1, str2))
 		puts("Congratulation.");
+		puts(begin);			//	return value errors.
+	}
 	else
 		puts("Bad luck.");
 
