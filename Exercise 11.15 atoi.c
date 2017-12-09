f211540f7c01af5f66ef30411f9a1c87360e7d40
赵子豪 <13969918264@163.com>
2017-12-09 21:31:45 +0800

    Update Exercise 11.15 atoi.c

--- Exercise 11.15 atoi.c
+++ Exercise 11.15 atoi.c
@@ -3,21 +3,52 @@
 	Date: 2017年12月04日
 */
 /*	atoi.c	*/
+/*	fail to run	*/
 #include <string.h>
 #include <ctype.h>
-int atoi(char * st)
+
+#define MAX 100
+
+int atoi_s(char *);
+
+int main(void)
 {
-	int flag = 0;
+	char str[MAX];
+	int i;
+
+	puts("Enter as you like, it will change character to integer.");
+	for (i = 0; i < MAX && (str[i] = getchar()) != '\n'; i++)
+		continue;
+	if (str[i] == '\n')
+		str[i] = '\0';
+	else
+		while (getchar() != '\n')
+			continue;
+	str[MAX - 1] = '\0';
+	puts("You entered");
+	puts(str);
+	atoi(str);
+	puts("Now, it is:");
+	puts(str);
+
+	return 0;
+}
+
+
+
+int atoi_s(char * st)
+{
+	int flag = 1;
 	int i = 0;
 
-	while (i < strlen(st))
+	while (*(st + i))
 	{
 		if (isdigit(*(st + i)))
-			flag = 1;
+			continue;
 		else
 		{
 			flag = 0;
-			*(st + i) = *(st + i) - '0';
+			atoi(*(st + i));
 		}
 		i++;
 	}
