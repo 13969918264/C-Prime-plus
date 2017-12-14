f1718a4a3ed6ac8e58cda5a750e1fee1cb2f6a57
赵子豪 <13969918264@163.com>
2017-12-14 16:46:52 +0800

    Create Exercise 12.3 re_pe12-2a.h

---
+++ Exercise 12.3 re_pe12-2a.h
@@ -0,0 +1,8 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2017年12月14日
+*/
+/*	re_pe12-2a.h -- declaration of function used by re_pe12-2b.c	*/
+int set_mode(int);
+void get_info(int, double *, double *);
+void show_info(int, double *, double *);
