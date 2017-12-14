00524dc051056ecb2ed08135aa71e5bc9953ee03
赵子豪 <13969918264@163.com>
2017-12-14 18:21:36 +0800

    Update Exercise 12.3 re_pe12-2a.h

--- Exercise 12.3 re_pe12-2a.h
+++ Exercise 12.3 re_pe12-2a.h
@@ -5,4 +5,4 @@
 /*	re_pe12-2a.h -- declaration of function used by re_pe12-2b.c	*/
 int set_mode(int);
 void get_info(int, double *, double *);
-void show_info(int, double *, double *);
+void show_info(int, double, double);
