eb16576c8b60d2e5b75aedc491b964817083327f
赵子豪 <13969918264@163.com>
2018-01-04 11:01:04 +0800

    Update Exercise 11.14 power_base_command_line.c

--- Exercise 11.14 power_base_command_line.c
+++ Exercise 11.14 power_base_command_line.c
@@ -3,11 +3,31 @@
 	Date: 2017年12月04日
 */
 /*	power_base_command_line.c	*/
+/*	sscanf() can read from a string. and i find it on the internet	*/
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(int argc, char * argv[])
 {
-	printf("%lf power to %d is %lf.", *argv[1], *argv[2], pow(*argv[1], *argv[2]));
+	int i = 0;
+	int exp;
+	double base;
+	double multi = 1;
+
+	if (argc < 3)
+	{
+		puts("There are no command-line arguments");
+		exit(EXIT_FAILURE);
+	}
+	sscanf(argv[1], "%lf", &base);
+	sscanf(argv[2], "%d", &exp);
+	while (i < exp)
+	{
+		multi *= base;
+		i++;
+	}
+	printf("%lf power to %d is %lf\n", base, exp, multi);
 
 	return 0;
 }
