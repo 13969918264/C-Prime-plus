260fced6a0b645fcf565836170ecc9fe305ef818
赵子豪 <13969918264@163.com>
2018-01-12 09:46:39 +0800

    Update Listing 14.1 book.c

--- Listing 14.1 book.c
+++ Listing 14.1 book.c
@@ -24,4 +24,33 @@ int main(void)
 	printf("Please enter the book title.\n");
 	s_gets(library.title, MAXTITL);			/*	access to the title portion	*/
 	printf("Now enter the author.\n");
-	s_gets(library.author, MAXAUTL);Y}
+	s_gets(library.author, MAXAUTL);
+	printf("Now enter the value.\n");
+	scanf("%f", &library.value);
+	printf("%s by %s: $%.2f\n", library.title, library.author, library.value);
+	printf("%s: \"%s\" ($%.2f)\n", library.author, library.title, library.value);
+	printf("Done.\n");
+
+	return 0;
+}
+
+
+
+char * s_gets(char * st, int n)
+{
+	char * ret_val;
+	char * find;
+
+	ret_val = fgets(st, n, stdin);
+	if (ret_val)
+	{
+		find = strchr(st, '\n');			//	look for newline
+		if (find)			//	if the address if not null
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;			//	dispose of rest of line
+	}
+
+	return ret_val;
+}
