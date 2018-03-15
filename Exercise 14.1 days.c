db50dfc533dd17e98456bb593bc50b7278bc7107
赵子豪 <13969918264@163.com>
2018-03-15 11:43:48 +0800

    Create Exercise 14.1 days.c

---
+++ Exercise 14.1 days.c
@@ -0,0 +1,158 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2018年3月15日
+*/
+/*	days.c -- given the month number and calculate the days up the the month(besides it)	*/
+#include <stdio.h>
+#include <string.h>
+
+#define MAXMONTH 12
+#define NAMESIZE 15
+#define ABBRE 4
+
+
+struct month {
+	char name[NAMESIZE];
+	char abbre[ABBRE];			//	a three-letter abbreviation
+	unsigned days;
+	unsigned number;
+};
+
+struct month monthInfo[MAXMONTH] = {
+	{
+		"Janurary",
+		"Jan",
+		31,
+		1
+	},
+	{
+		"February",
+		"Feb",
+		28,
+		2
+	},
+	{
+		"March",
+		"Mar",
+		31,
+		3
+	},
+	{
+		"April",
+		"Apr",
+		30,
+		4
+	},
+	{
+		"May",
+		"May",
+		31,
+		5
+	},
+	{
+		"June",
+		"Jun",
+		30,
+		6
+	},
+	{
+		"July",
+		"Jul",
+		31,
+		7
+	},
+	{
+		"August",
+		"Aug",
+		31,
+		8
+	},
+	{
+		"September",
+		"Sep",
+		30,
+		9
+	},
+	{
+		"Octomber",
+		"Oct",
+		31,
+		10
+	},
+	{
+		"November",
+		"Nov",
+		30,
+		11
+	},
+	{
+		"December",
+		"Dec",
+		31,
+		12
+	}
+};
+
+char * s_gets(char *, int);
+int calculate(struct month *, char *);
+
+int main(void)
+{
+	char name[NAMESIZE];
+	int sum_days;
+	int index = 0;
+
+	printf("Please enter the spelled-out name of a month.\n");
+	while (s_gets(name, NAMESIZE) != NULL && name[0] != '\0')
+	{
+		while (index < MAXMONTH)
+			if (strcmp(name, monthInfo[index++].name) == 0)
+				break;
+		if (index == MAXMONTH)
+		{
+			printf("Enter the correct name.\n");
+			continue;
+		}
+		sum_days = calculate(monthInfo, name);
+		printf("up to %s is %d days.\n", name, sum_days);
+		printf("the next month name? newline to quit\n");
+	}
+
+	return 0;
+}
+
+
+
+char * s_gets(char * st, int n)
+{
+	char * ret_val;
+	char * find;
+
+	ret_val = fgets(st, n, stdin);
+	if (ret_val)
+	{
+		if (find = strchr(st, '\n'))
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;
+	}
+
+	return ret_val;
+}
+
+
+
+int calculate(struct month * mon, char * st)
+{
+	int sum_days = 0;
+
+	while (*st != mon->name)
+	{
+		sum_days += mon->days;
+		mon++;
+	}
+	sum_days += mon->days;
+
+	return sum_days;
+}
