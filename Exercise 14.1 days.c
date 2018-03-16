00f669820ddfa212a53029db99b4fe08613244ad
赵子豪 <13969918264@163.com>
2018-03-16 09:55:08 +0800

    Update Exercise 14.1 days.c

--- Exercise 14.1 days.c
+++ Exercise 14.1 days.c
@@ -94,17 +94,17 @@ struct month monthInfo[MAXMONTH] = {
 };
 
 char * s_gets(char *, int);
-int calculate(struct month *, char *);
 
 int main(void)
 {
 	char name[NAMESIZE];
-	int sum_days;
+	int sum_days = 0;
 	int index = 0;
 
 	printf("Please enter the spelled-out name of a month.\n");
-	while (s_gets(name, NAMESIZE) != NULL)
+	while (s_gets(name, NAMESIZE) != NULL && name[0] != '\0')
 	{
+		puts(name);
 		while (index < MAXMONTH)
 			if (strcmp(name, monthInfo[index++].name) == 0)
 				break;
@@ -113,7 +113,7 @@ int main(void)
 			printf("Enter the correct name.\n");
 			continue;
 		}
-		sum_days = calculate(monthInfo, name);
+		printf("%d\n", index);			//	test
 		printf("up to %s is %d days.\n", name, sum_days);
 		printf("the next month name? newline to quit\n");
 	}
@@ -140,20 +140,4 @@ char * s_gets(char * st, int n)
 	}
 
 	return ret_val;
-}
-
-
-
-int calculate(struct month * mon, char * st)
-{
-	int sum_days = 0;
-
-	while (*st != mon->name)
-	{
-		sum_days += mon->days;
-		mon++;
-	}
-	sum_days += mon->days;
-
-	return sum_days;
 }
