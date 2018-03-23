8aa4805372b7ae968598ba76222d1db8464721a7
赵子豪 <13969918264@163.com>
2018-03-23 11:20:57 +0800

    Create Exercise 14.3 manybook.c

---
+++ Exercise 14.3 manybook.c
@@ -0,0 +1,83 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date：2018年3月23日
+*/
+/*	manybook.c -- multiple book inventory	*/
+#include <stdio.h>
+#include <string.h>
+
+#define MAXTITL 40
+#define MAXAUTL 40
+#define MAXBKS 100			/*	maximum number of books	*/
+
+struct book{			/*	set up book template	*/
+	char title[MAXTITL];
+	char author[MAXAUTL];
+	float value;
+};
+
+char * s_gets(char *, int);
+struct book (*) [] sort(struct book (*) [], char *);
+
+int main(void)
+{
+	struct book library[MAXBKS];			/*	array of book structures	*/
+	int count = 0;
+	int index;
+	struct book (* ptstr)[MAXBKS];			/*	A pointer point to array of MAXBKS book structures	*/
+
+	printf("Please enter the book title.\n");
+	printf("Press [enter] at the start of a line to stop.\n");
+	while (count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL && library[count].title[0] != '\0')
+	{
+		printf("Now enter the author.\n");
+		s_gets(library[count].author, MAXAUTL);
+		printf("Now enter the value.\n");
+		scanf("%f", &library[count++].value);
+		while (getchar() != '\n')
+			continue;			/*	clear input line	*/
+		if (count < MAXBKS)
+			printf("Enter the next title.\n");
+	}
+
+	if (count > 0)
+	{
+		printf("Here is the list of you books:\n");
+		for (index = 0; index < count; index++)
+			printf("%s by %s: $%.2f\n", library[index].title, library[index].author, library[index].value);
+		ptstr = library;
+	//	In order of alphabetized title
+		sort(ptstr, title);
+		for (index = 0; index < count; index++)
+			printf("%s by %s: $%.2f\n", ptstr[index].title, ptstr[index].authour, ptstr[index].value);
+	//	In order of increased value
+		sort(ptstr, value)
+		for (index = 0; index < count; index++)
+			printf("%s by %s: $%.2f\n", ptstr[index].title, ptstr[index].authour, ptstr[index].value);
+	}
+	else
+		printf("No books? Too bad");
+
+	return 0;
+}
+
+
+
+char * s_gets(char * st, int n)
+{
+	char * ret_val;
+	char * find;
+
+	ret_val = fgets(st, n, stdin);
+	if (ret_val)
+	{
+		find = strchr(st, '\n');
+		if (find)
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;
+	}
+
+	return ret_val;
+}
