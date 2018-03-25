d49de4028622058d1ebc7f1d011d44552cb3db49
赵子豪 <13969918264@163.com>
2018-03-25 21:43:51 +0800

    Create Exercise 14.4.a person.c

---
+++ Exercise 14.4.a person.c
@@ -0,0 +1,75 @@
+/*
+	Name: 赵子豪(ZiHao Zhao)
+	Date: 2018年3月25日
+*/
+/*	person.c -- create a struct to restore information of mankind	*/
+#include <stdio.h>
+
+#define MAXSIZE 5			//	array size
+#define MAXDIGIT 19			//	for security number
+#define NAMESIZE 41			//	for name
+
+struct name {
+	char first[NAMESIZE];
+	char middle[NAMESIZE];
+	char last[NAMESIZE];
+};
+
+struct person {
+	char security_number[MAXDIGIT];
+	struct name who;
+};
+
+void print_str(struct person [], int);
+
+int main(void)
+{
+	struct person people[MAXSIZE] = {
+		{	"2015072050",
+			{	"Hao",
+				"Zi",
+				"Zhao"
+			}
+		},
+		{	"19951203",
+			{	"Ting",
+				"Ting",
+				"Li"
+			}
+		},
+		{	"19960704",
+			{	"Jing",
+				"Wen",
+				"Li"
+			}
+		},
+		{	"20170702",
+			{	"sister",
+				"",
+				"senior"
+			}
+		},
+		{	"20180324",
+			{	"two",
+				"Senior mates",
+				"Long leg and beautiful face"
+			}
+		}
+	};
+
+	printf("Now, It will print the array of structures to screen.\n");
+	print_str(people, MAXSIZE);
+	printf("Done.\n");
+
+	return 0;
+}
+
+
+
+void print_str(struct person str[], int n)
+{
+	int index = 0;
+
+	for (index; index < n; index++)
+		printf("%s %s %c. -- %s\n", str[index].who.first, str[index].who.last, str[index].who.middle[0], str[index].security_number);
+}
