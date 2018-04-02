9112dc38705beba0e1b7fd31276f20b469f84229
赵子豪 <13969918264@163.com>
2018-04-02 22:28:24 +0800

    Update Exercise 14.6 player.c

--- Exercise 14.6 player.c
+++ Exercise 14.6 player.c
@@ -184,14 +184,14 @@ int main(void)
 			exit(EXIT_FAILURE);
 		}
 		getc(ptf);
-		printf("Here is %d.\n", index);
+	//	printf("Here is %d.\n", index);
 		access_calculate(ptf, &players[index]);
 	}
 	printf("Access and calculate successfully.\n");
 	fprintf(stdout, "Now, there are the scores of players.\n");
 	printf("number	first name	last name	bats	hits	walks	rpis	aver_bat\n");
 	for (index = 0; index < MAXNUM; index++)
-		printf("%d	%s	%s			%d	%d	%d	%d	%f\n",
+		printf("%d	%s		%s		%d	%d	%d	%d	%f\n",
 			index, players[index].first, players[index].last, players[index].bats, players[index].hits,	players[index].walks, players[index].rpis, players[index].bat_aver);
 	if (fclose(ptf) != 0)
 		fprintf(stderr, "Error in closing files\n");
@@ -228,33 +228,34 @@ void access_calculate(FILE * ptf, struct scores * pts)
 	unsigned int walks = 0;
 	unsigned int rpis = 0;
 
-	printf("Access first name of a player.\n");
+//	printf("Access first name of a player.\n");
 	for (index = 0; (ch = getc(ptf)) != ' ' && index < NAMESIZE; index++)
 		pts->first[index] = ch;
 	pts->first[index] = '\0';
-	printf("Access last name of a player.\n");
+//	printf("Access last name of a player.\n");
 	for (index = 0; (ch = getc(ptf)) != ' ' && index < NAMESIZE; index++)
 		pts->last[index] = ch;
 	pts->last[index] = '\0';			//	produce a string, not an array
-	printf("%s %s\n", pts->first, pts->last);
-	printf("Access the bats.\n");
+//	printf("%s %s\n", pts->first, pts->last);
+//	printf("Access the bats.\n");
 	fscanf(ptf, "%u", &bats);
 	pts->bats += bats;
-	printf("bats: %u\n", pts->bats);
-	printf("Access the hits.\n");
+//	printf("bats: %u\n", pts->bats);
+//	printf("Access the hits.\n");
 	fscanf(ptf, "%u", &hits);
 	pts->hits += hits;
-	printf("hits: %u\n", pts->hits);
-	printf("Access the walks.\n");
+//	printf("hits: %u\n", pts->hits);
+//	printf("Access the walks.\n");
 	fscanf(ptf, "%u", &walks);
 	pts->walks += walks;
-	printf("Access the rpis.\n");
+//	printf("Access the rpis.\n");
 	fscanf(ptf, "%u", &rpis);
 	pts->rpis += rpis;
-	printf("Calculate the average of bats.\n");
+//	printf("Calculate the average of bats.\n");
 //	There is a bug, I will find it.
 //	I have found it. In this case, when it read from the file, i forgot to let it omit a space,
 //	and ensue the content in the file was overlooked by this program, because of my design.
-	pts->bat_aver = pts->hits / pts->bats;
-	printf("Done!\n");
+	pts->bat_aver = (float)((float)pts->hits / (float)pts->bats);			//	we should cast a integer to float
+//	printf("%f\n", pts->bat_aver);
+//	printf("Done!\n");
 }
