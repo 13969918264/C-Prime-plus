8f465978a43848d31263e1df0343634ef918d1ba
赵子豪 <13969918264@163.com>
2019-07-16 20:06:50 +0800

    Create Listing 15.5 align.c

---
+++ Listing 15.5 align.c
@@ -0,0 +1,29 @@
+/*
+	姓名：赵子豪
+	日期：2019年7月16日
+*/
+
+//	align.c -- using _Alignof and _Alignas (C11)
+
+#include <stdio.h>
+
+int main(void)
+{
+	double dx;
+	char ca;
+	char cx;
+	double dz;
+	char cb;
+	char _Alignas(double) cz;
+
+	printf("char alignment:		%zd\n", _Alignof(char));
+	printf("double alignment:	%zd\n", _Alignof(double));
+	printf("&dx: %p\n", &dx);
+	printf("&ca: %p\n", &ca);
+	printf("&cx: %p\n", &cx);
+	printf("&dz: %p\n", &dz);
+	printf("&cb: %p\n", &cb);
+	printf("&cz: %p\n", &cz);
+
+	return 0;
+}
