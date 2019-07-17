32524762fec70e41f9c7b1778d87071de04aa7b1
赵子豪 <13969918264@163.com>
2019-07-17 21:13:02 +0800

    Create Listing 16.1 preproc.c

---
+++ Listing 16.1 preproc.c
@@ -0,0 +1,25 @@
+/*
+	姓名：赵子豪
+	日期：2019年7月17日
+*/
+/*	preproc.c -- simple preprocessor example	*/
+
+#include <stdio.h>
+
+#define TWO 2			/*	you can use comments if you like	*/
+#define OW "Consisteny is the last refusing of the unimagina\tive. -- Oscar Wilde"	/*	a backslash continue a definition to the next line	*/
+#define FOUR  TWO*TWO
+#define PX printf("X is %d.\n", x)
+
+int main(void)
+{
+	int x = TWO;
+
+	PX;
+	x = FOUR;
+	printf(FMT, x);
+	printf("%s\n", OW);
+	printf("TWO: OW\n");
+
+	return 0;
+}
