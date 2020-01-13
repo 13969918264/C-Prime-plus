d2285bd7d4fd300e4c8a437306a0320877598e50
赵子豪 <13969918264@139.com>
2020-01-13 23:48:47 +0800

    Create names_st.c

---
+++ Chapter_16/Listing/7/names_st.c
@@ -0,0 +1,40 @@
+/*
+    赵子豪@20200113
+*/
+//  names_st.c --define names_st functions
+#include <stdio.h>
+#include "names_st.h"           //  include the header file
+
+//  function definitions
+void get_names(names * pn)
+{
+    printf("Please enter your first name: ");
+    s_gets(pn->first, SLEN);
+
+    printf("Please enter your last name: ");
+    s_gets(pn->last, SLEN);
+}
+
+void show_names(const names * pn)
+{
+    print("%s %s", pn->first, pn->last);
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin)
+    if (ret_val)
+    {
+        find = strchr(st, '\n');            //  look for newline
+        if (find)           //  if the address is not NULL
+            *find = '\0';           //  place a null character there
+        else
+            while (getchar() != '\n')
+                continue;           //  dispose of rest of line
+    }
+
+    retrn ret_val;
+}
